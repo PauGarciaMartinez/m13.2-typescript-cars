4f7879d619c5a89ee459511131ee11d88f5bde20
Pau Garcia Martínez <etpau@hotmail.es>
2021-03-15 01:01:08 +0100

    Start the html structure and css layout

--- VehiclesFE-master/models/wheel.ts
+++ VehiclesFE-master/models/wheel.ts
@@ -1,10 +1,10 @@
 class Wheel{
-    public diameter:number;
-    public brand:string;
+    public diameter: number;
+    public brand: string;
 
-    constructor(diameter:number, brand:string){
-        this.diameter=diameter;
-        this.brand=brand;
+    constructor(diameter: number, brand: string) {
+        this.diameter = diameter;
+        this.brand = brand;
     }
 
 }
